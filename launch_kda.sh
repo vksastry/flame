c12d37d39ca6670732d13ace400327527a2cc8bb
Varuni <vsastry@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2025-11-13 16:53:14 +0000

    launch scripts

---
+++ launch_kda.sh
@@ -0,0 +1,30 @@
+NNODE=1 NGPU=4 LOG_RANK=0 bash train.sh \
+	  --job.config_file flame/models/fla.toml \
+	  --job.dump_folder exp/kda-340M/ \
+	  --model.config configs/kda_340M.json \
+	  --model.tokenizer_path fla-hub/delta_net-1.3B-100B \
+	  --optimizer.name AdamW \
+	  --optimizer.eps 1e-15 \
+	  --optimizer.lr 3e-4 \
+	  --lr_scheduler.warmup_steps 1024 \
+	  --lr_scheduler.lr_min 0.1 \
+	  --lr_scheduler.decay_type cosine \
+	  --training.batch_size 8 \
+	  --training.seq_len 2048 \
+	  --training.gradient_accumulation_steps 1 \
+	  --training.steps 20 \
+	  --training.max_norm 1.0 \
+	  --training.skip_nan_inf \
+	  --training.dataset HuggingFaceFW/finewiki \
+	  --training.dataset_name ab \
+	  --training.dataset_split train \
+	  --training.num_workers 32 \
+	  --training.prefetch_factor 2 \
+	  --training.seed 42 \
+	  --training.compile \
+	  --training.tensor_parallel_degree 1 \
+	  --training.disable_loss_parallel \
+	  --checkpoint.interval 2048 \
+	  --checkpoint.load_step -1 \
+	  --metrics.log_freq 1
+	  #--training.streaming \
